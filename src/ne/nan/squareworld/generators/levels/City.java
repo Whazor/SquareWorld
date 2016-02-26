469885be2f0f8244ff9640421958f92b8d24b6bb
otherwise777 <commits@onzichtbaar.net>
2016-02-26 16:41:53 +0100

    ideas

---
+++ src/ne/nan/squareworld/generators/levels/City.java
@@ -0,0 +1,14 @@
+package ne.nan.squareworld.generators.levels;
+
+/**
+ * Created by s133781 on 26-2-16.
+ */
+public class City {
+//    constructor met settlement
+//    x,y
+//    seed
+/*
+    idea: take 1 size for the city, generate couple of random nodes in the graph with x,y values, then generate the main roads based on those variabls
+     */
+
+}
