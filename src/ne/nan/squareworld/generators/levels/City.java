a6d6ca20a3669fc9c7134e6e686487a8f8cccbc2
otherwise777 <commits@onzichtbaar.net>
2016-03-11 12:15:27 +0100

    small change

--- src/ne/nan/squareworld/generators/levels/City.java
+++ src/ne/nan/squareworld/generators/levels/City.java
@@ -1,14 +1,32 @@
 package ne.nan.squareworld.generators.levels;
 
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by s133781 on 26-2-16.
  */
 public class City {
 //    constructor met settlement
 //    x,y
+
 //    seed
 /*
     idea: take 1 size for the city, generate couple of random nodes in the graph with x,y values, then generate the main roads based on those variabls
+
+     we have a list @coordinates with all the random coordinates in the plane
      */
+    int x = 15;
+    int y = 15;
+
+    private final List<Point2D> coordinates = new ArrayList<Point2D>();
+    for 
+    public boolean isThereAWall(Point2D userPosition) {
+        return coordinates.contains(userPosition);
+    }
+
+
+
 
 }
