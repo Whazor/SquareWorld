daed15569493b45e2462306fdaeb2bf20646eb14
otherwise777 <commits@onzichtbaar.net>
2016-03-20 14:50:37 +0100

    city command added

--- src/ne/nan/squareworld/createCityCommandExecutor.java
+++ src/ne/nan/squareworld/createCityCommandExecutor.java
@@ -1,5 +1,9 @@
 package ne.nan.squareworld;
 
+import ne.nan.squareworld.generators.levels.City;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -28,12 +32,35 @@ public class createCityCommandExecutor implements CommandExecutor{
                     sender.sendMessage("Not enough arguments!");
                     return false;
                 }
-
-
-
-
                 System.out.println("Starting city creation");
                 Player player = (Player) sender;
+
+//                generate the city in destad
+                City stad = new City(1234,0,0,500,500);
+                short[][] destad = stad.generate();
+
+//                get the players location and store them in x z integers
+
+                Location loc = player.getLocation();
+                int lx = (int) loc.getX();
+                int lz = (int) loc.getZ();
+
+                // Sets the block to type id 1 (stone).
+                for (int x = 0; x < destad.length; x++) {
+                    short[] xrow = destad[x];
+                    for (int z = 0; z < xrow.length; z++) {
+                        short value = xrow[z];
+
+                        Location newloc = loc;
+                        newloc.setX(loc.getX() + x);
+                        newloc.setZ(loc.getZ() + z);
+
+                        System.out.println("newloc = " +newloc);
+
+                        Block b = newloc.getBlock();
+                        b.setType(Material.getMaterial(value));
+                    }
+                }
 
                 return true;
                 // do something
